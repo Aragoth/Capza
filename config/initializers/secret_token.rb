ea8362d22cd21c9b225c98e9b8d488128fe10593
Aragoth <tharnid@gmail.com>
2011-09-21 23:46:20 -0400

    Intial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capza::Application.config.secret_token = 'eab62226470ee327c96b79bd1164a29de99c006d3943ed03f414443514fba8ad344f9cc32a9ccb587cd7f2f5a3125b47dcdca0fcddfb6ecce5c2e82d7229f193'
